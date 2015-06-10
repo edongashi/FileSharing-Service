d550491aa6e71109db102509a28f2cbfb327c51a
EdonGashi <edongashi@hotmail.co.uk>
2015-06-10 06:04:20 +0200

    Klienti UI
    Templating per lista ka met

---
+++ src/Klienti/KlientiUI/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.Controls;
+
+namespace KlientiUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //System.IO.FileSystemWatcher
+        }
+    }
+}
